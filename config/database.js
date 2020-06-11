c2277fa6db29c6e925543511475c198d7db47688
Leonardo Souza <llsouza.br@gmail.com>
2020-06-11 14:24:55 -0300

    Ajustes locais

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
-module.exports = {
-  username: "root",
-  password: null,
-  database: "projeto_doacao",
-  host: "127.0.0.1",
-  dialect: "mysql",
-};
+require('dotenv').config();
+
+const config = {
+    database: process.env.DB_NAME || 'projeto_doacao',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    timestamps: true
+}
+
+module.exports = config;
